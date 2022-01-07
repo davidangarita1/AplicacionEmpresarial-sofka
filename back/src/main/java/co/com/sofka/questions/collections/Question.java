7a5d313ae7d937f1da29aa8aa6cc7c00c363c13a
davidangarita1 <davidangarita.sistemas@gmail.com>
2022-01-06 19:16:41 -0500

    se agrega test createUseCaseTest

--- back/src/main/java/co/com/sofka/questions/collections/Question.java
+++ back/src/main/java/co/com/sofka/questions/collections/Question.java
@@ -2,10 +2,15 @@ package co.com.sofka.questions.collections;
 
 import co.com.sofka.questions.utils.Category;
 import co.com.sofka.questions.utils.Type;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
-
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
 @Document
 public class Question {
     @Id
--- back/src/test/java/co/com/sofka/questions/useCases/DeleteUseCaseTest.java
+++ back/src/test/java/co/com/sofka/questions/useCases/DeleteUseCaseTest.java
@@ -1,5 +1,8 @@
 package co.com.sofka.questions.useCases;
 
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
 class DeleteUseCaseTest {
 
 }
